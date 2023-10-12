c10cd17c97914e7d334fc7f80ffe5fc7aa6c538a
BenjaminGraziadei223946 <223946@buas.nl>
2023-10-12 10:00:13 +0200

    Update VoiceBot.py

--- VoiceBot/VoiceBot.py
+++ VoiceBot/VoiceBot.py
@@ -47,13 +47,8 @@ def audio_output(output):
     sound_b64 = base64.b64encode(audio_byte_io.getvalue()).decode("utf-8")
     audio_html = f'<audio id="audioElement" controls autoplay><source src="data:audio/mp3;base64,{sound_b64}"></audio>'
     audio.markdown(audio_html, unsafe_allow_html=True)
-    js_test = """
-                <script language="javascript">
-                    alert("this is a test!");
-                    console.log("test");
-                </script>
-            """
-    html(js_test, height=600, width=490)
+    st.markdown(speak_js, unsafe_allow_html=True)
+    
 
 
 
